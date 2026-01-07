8677a4d699936b40026cea24db143dfee4ffaeda
TNHdev123 <TNHdev123@outlook.com>
2026-01-07 23:57:27 +0800

    Create repo.js

---
+++ aios/nitweaks/repo.js
@@ -0,0 +1,26 @@
+{
+  "tweaks": [
+    {
+      "id": "lockfix",
+      "name": "Lock Fix",
+      "description": "Real Lock Screen with Passcode on AIOS!",
+      "icon": "https://example.com/example.png",
+      "iconColor": "#00d2ff",
+      "url": "https://tnhdev123.github.io/site/aios/nitweaks/lockfix.js",
+      "category": "Tool",
+      "type": "js",
+      "fallbackIcon": "example"
+    },
+    {
+      "id": "test",
+      "name": "Test",
+      "description": "Example",
+      "icon": "https://example.com/example.png",
+      "iconColor": "#5d3fd3",
+      "url": "https://tnhdev123.github.io/site/aios/nitweaks/test.js",
+      "category": "Test",
+      "type": "js",
+      "fallbackIcon": "example"
+    }
+  ]
+}
